da381e6a09e1f7125b49a4c3e14ad3433365e265
Brendan Grubb <brendangrubb@gmail.com>
2017-02-24 14:40:40 -0800

    add and test accurate category id to Client getters and setters

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -5,8 +5,8 @@
     * @backupStaticAttributes disabled
     */
 
-    require_once __DIR__.'/../src/Stylist.php';
     require_once __DIR__.'/../src/Client.php';
+    require_once __DIR__.'/../src/Stylist.php';
 
     $server = 'mysql:host=localhost:8889;dbname=hair_salon_test';
     $username = 'root';
@@ -18,16 +18,25 @@
     {
         protected function tearDown()
         {
+            Stylist::deleteAllStylists();
             Client::deleteAllClients();
         }
     // GETTERS AND SETTERS TESTS
 
         function test_getId()
         {
+            $id = null;
+            $name = 'Jacques St Gerrard';
+            $phone_number = '5559991234';
+            $workdays= 'Monday, Saturday';
+            $new_stylist = new Stylist($id, $name, $phone_number, $workdays);
+            $new_stylist->saveStylist();
+
+
             $id = 1;
             $name = 'Vince Neil';
             $phone_number = '2125436789';
-            $stylist_id= 1;
+            $stylist_id= $new_stylist->getId();
             $new_client = new Stylist($id, $name, $phone_number, $stylist_id);
 
             $result = $new_client->getId();
@@ -37,10 +46,17 @@
 
         function test_getName()
         {
+            $id = null;
+            $name = 'Jacques St Gerrard';
+            $phone_number = '5559991234';
+            $workdays= 'Monday, Saturday';
+            $new_stylist = new Stylist($id, $name, $phone_number, $workdays);
+            $new_stylist->saveStylist();
+
             $id = 1;
             $name = 'Vince Neil';
             $phone_number = '2125436789';
-            $stylist_id= 1;
+            $stylist_id= $new_stylist->getId();
             $new_client = new Stylist($id, $name, $phone_number, $stylist_id);
 
             $result = $new_client->getName();
@@ -50,10 +66,17 @@
 
         function test_getPhoneNumber()
         {
+            $id = null;
+            $name = 'Jacques St Gerrard';
+            $phone_number = '5559991234';
+            $workdays= 'Monday, Saturday';
+            $new_stylist = new Stylist($id, $name, $phone_number, $workdays);
+            $new_stylist->saveStylist();
+
             $id = 1;
             $name = 'Vince Neil';
             $phone_number = '2125436789';
-            $stylist_id= 1;
+            $stylist_id= $new_stylist->getId();
             $new_client = new Stylist($id, $name, $phone_number, $stylist_id);
 
             $result = $new_client->getPhoneNumber();
@@ -63,15 +86,22 @@
 
         function test_getStylistId()
         {
+            $id = null;
+            $name = 'Jacques St Gerrard';
+            $phone_number = '5559991234';
+            $workdays= 'Monday, Saturday';
+            $new_stylist = new Stylist($id, $name, $phone_number, $workdays);
+            $new_stylist->saveStylist();
+
             $id = 1;
             $name = 'Vince Neil';
             $phone_number = '2125436789';
-            $stylist_id= 1;
+            $stylist_id= $new_stylist->getId();
             $new_client = new Client($id, $name, $phone_number, $stylist_id);
 
             $result = $new_client->getStylistId();
 
-            $this->assertEquals(1, $result);
+            $this->assertEquals($stylist_id, $result);
         }
     //CRUD METHODS
         function test_saveClient()
