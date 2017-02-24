d034af108a7905da7beb6ee6d756bf8484ec2448
Brendan Grubb <brendangrubb@gmail.com>
2017-02-24 14:22:11 -0800

    add functionality to delete entry of specific client from the database

--- src/Client.php
+++ src/Client.php
@@ -67,7 +67,7 @@
 
         function deleteClient()
         {
-            
+            $GLOBALS['DB']->exec("DELETE FROM clients WHERE id = {$this->getId()};");              
         }
 
         static function getAllClients()
