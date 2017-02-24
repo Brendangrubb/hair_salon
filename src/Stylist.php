18bbc7b38ef9a8a92cccd136528961daaf7ae7c8
Brendan Grubb <brendangrubb@gmail.com>
2017-02-24 09:25:54 -0800

    add dependencies to composer file and stylist class with test page

---
+++ src/Stylist.php
@@ -0,0 +1,35 @@
+<?php
+    class Stylist
+    {
+        private $id;
+        private $name;
+        private $phone_number;
+        private $workdays;
+
+        function __construct($id = null, $name, $phone_number, $workdays)
+        {
+            $this->id = $id;
+            $this->name = $name;
+            $this->phone_number = $phone_number;
+            $this->workdays = $workdays;
+        }
+
+        function getId()
+        {
+
+        }        
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+?>
---
+++ tests/StylistTest.php
@@ -0,0 +1,29 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once __DIR__.'/../src/Stylist.php';
+
+    $server = 'mysql:host=localhost:8889;dbname=hair_salon_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+
+    class StylistTest extends PHPUnit_Framework_TestCase
+    {
+
+
+
+
+
+
+
+
+
+
+    }
+?>
