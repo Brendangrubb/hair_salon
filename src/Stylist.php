2b773afeb478dc0a50f1ae02b1126eef6db50d1c
Brendan Grubb <brendangrubb@gmail.com>
2017-02-24 09:35:02 -0800

    add functionality to get Stylist phone number

--- src/Stylist.php
+++ src/Stylist.php
@@ -33,11 +33,13 @@
 
         function setPhoneNumber($new_phone_number)
         {
+            $this->phone_number = $new_phone_number;
 
         }
 
         function getPhoneNumber()
         {
+            return $this->phone_number;
 
         }
 
