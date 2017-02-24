68cd9a93c4eef425e62198ecac10cac1d3246add
Brendan Grubb <brendangrubb@gmail.com>
2017-02-24 10:47:56 -0800

    add test and functionality to list all stylists entered in the database

--- tests/StylistTest.php
+++ tests/StylistTest.php
@@ -16,76 +16,99 @@
     class StylistTest extends PHPUnit_Framework_TestCase
     {
     //GETTERS AND SETTERS TESTS
-        function test_getId()
+    //     function test_getId()
+    //     {
+    //         $id = 1;
+    //         $name = 'Jacques St Gerrard';
+    //         $phone_number = '5559991234';
+    //         $$workdays= 'Monday, Saturday';
+    //         $new_stylist = new Stylist($id, $name, $phone_number, $workdays);
+    //
+    //         $result = $new_stylist->getId();
+    //
+    //         $this->assertEquals(1, $result);
+    //     }
+    //
+    //
+    //     function test_getName()
+    //     {
+    //         $id = 1;
+    //         $name = 'Jacques St Gerrard';
+    //         $phone_number = '5559991234';
+    //         $$workdays= 'Monday, Saturday';
+    //         $new_stylist = new Stylist($id, $name, $phone_number, $workdays);
+    //
+    //         $result = $new_stylist->getName();
+    //
+    //         $this->assertEquals('Jacques St Gerrard', $result);
+    //     }
+    //
+    //     function test_getPhoneNumber()
+    //     {
+    //         $id = 1;
+    //         $name = 'Jacques St Gerrard';
+    //         $phone_number = '5559991234';
+    //         $$workdays= 'Monday, Saturday';
+    //         $new_stylist = new Stylist($id, $name, $phone_number, $workdays);
+    //
+    //         $result = $new_stylist->getPhoneNumber();
+    //
+    //         $this->assertEquals('5559991234', $result);
+    //     }
+    //
+    //     function test_getWorkdays()
+    //     {
+    //         $id = 1;
+    //         $name = 'Jacques St Gerrard';
+    //         $phone_number = '5559991234';
+    //         $workdays= 'Monday, Saturday';
+    //         $new_stylist = new Stylist($id, $name, $phone_number, $workdays);
+    //
+    //         $result = $new_stylist->getWorkdays();
+    //
+    //         $this->assertEquals('Monday, Saturday', $result);
+    //     }
+    // //CRUD METHODS
+    //     function test_saveStylist()
+    //     {
+    //         $id = null;
+    //         $name = 'Jacques St Gerrard';
+    //         $phone_number = '5559991234';
+    //         $workdays= 'Monday, Saturday';
+    //         $new_stylist = new Stylist($id, $name, $phone_number, $workdays);
+    //
+    //         $new_stylist->saveStylist();
+    //
+    //         $get_all_array = array();
+    //         $result = array();
+    //         array_push($result, $new_stylist);
+    //         $get_all_array = Stylist::getAllStylists();
+    //         $this->assertEquals($get_all_array, $result);
+    //     }
+
+        function test_getAllStylists()
         {
-            $id = 1;
-            $name = 'Jacques St Gerrard';
-            $phone_number = '5559991234';
-            $$workdays= 'Monday, Saturday';
-            $new_stylist = new Stylist($id, $name, $phone_number, $workdays);
+            $result = array();
 
-            $result = $new_stylist->getId();
-
-            $this->assertEquals(1, $result);
-        }
-
-
-        function test_getName()
-        {
-            $id = 1;
-            $name = 'Jacques St Gerrard';
-            $phone_number = '5559991234';
-            $$workdays= 'Monday, Saturday';
-            $new_stylist = new Stylist($id, $name, $phone_number, $workdays);
-
-            $result = $new_stylist->getName();
-
-            $this->assertEquals('Jacques St Gerrard', $result);
-        }
-
-        function test_getPhoneNumber()
-        {
-            $id = 1;
-            $name = 'Jacques St Gerrard';
-            $phone_number = '5559991234';
-            $$workdays= 'Monday, Saturday';
-            $new_stylist = new Stylist($id, $name, $phone_number, $workdays);
-
-            $result = $new_stylist->getPhoneNumber();
-
-            $this->assertEquals('555-999-1234', $result);
-        }
-
-        function test_getWorkdays()
-        {
-            $id = 1;
-            $name = 'Jacques St Gerrard';
-            $phone_number = '5559991234';
-            $workdays= 'Monday, Saturday';
-            $new_stylist = new Stylist($id, $name, $phone_number, $workdays);
-
-            $result = $new_stylist->getWorkdays();
-
-            $this->assertEquals('Monday, Saturday', $result);
-        }
-    //CRUD METHODS
-        function test_saveStylist()
-        {
             $id = null;
             $name = 'Jacques St Gerrard';
             $phone_number = '5559991234';
             $workdays= 'Monday, Saturday';
             $new_stylist = new Stylist($id, $name, $phone_number, $workdays);
-
             $new_stylist->saveStylist();
 
-            $get_all_array = array();
-            $result = array();
-            array_push($result, $new_stylist);
-            $get_all_array = Stylist::getAllStylists();
-            $this->assertEquals($get_all_array, $result);
-        }
+            $id2 = null;
+            $name2 = 'Cristiano Francois';
+            $phone_number2 = '5038765309';
+            $workdays2 = 'Thursday, Friday';
+            $new_stylist2 = new Stylist($id2, $name2, $phone_number2, $workdays2);
+            $new_stylist2->saveStylist();
 
+
+            $get_all_stylists = Stylist::getAllStylists();
+
+            $this->assertEquals([$new_stylist->getName, $new_stylist2->getName], [$get_all_stylists[0]->getName, $get_all_stylists[1]->getName]);
+        }
 
 
 
