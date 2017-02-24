ae98e26336749460f8c2c184f51c92122a9aae8e
Brendan Grubb <brendangrubb@gmail.com>
2017-02-24 12:05:30 -0800

    add test and functionality to delete all stylists entered in the database

--- src/Client.php
+++ src/Client.php
@@ -69,7 +69,6 @@
                 $stylist_id = $client['stylist_id'];
                 $new_client = new Client($id, $name, $phone_number, $stylist_id);
 
-
                 array_push($all_clients, $new_client);
             }
 
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -112,6 +112,26 @@
             $this->assertEquals([$new_client->getName(), $new_client2->getName()], [$get_all_clients[0]->getName(), $get_all_clients[1]->getName()]);
         }
 
+        function test_deleteAllClients()
+        {
+            $id = null;
+            $name = 'Vince Neil';
+            $phone_number = '2125436789';
+            $stylist_id= 1;
+            $new_client = new Client($id, $name, $phone_number, $stylist_id);
+            $new_client->saveClient();
 
+            $id2 = null;
+            $name2 = 'Crispin Glover';
+            $phone_number2 = '9876345212';
+            $stylist_id2 = 1;
+            $new_client2 = new Client($id, $name, $phone_number, $stylist_id);
+            $new_client2->saveClient();
+
+            Client::deleteAllClients();
+
+            $get_all = Client::getAllClients();
+            $this->assertEquals([], $get_all);
+        }
     }
 ?>
